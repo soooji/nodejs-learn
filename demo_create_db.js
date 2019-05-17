089a565f67c75a416d1fca7a08e27c4ded0df196
Sajad Beheshti <beheshtisajad@gmail.com>
2019-05-17 14:25:34 +0430

    d

--- demo_create_db.js
+++ demo_create_db.js
@@ -2,7 +2,7 @@ var dbcon = require('./demo_db_connection');
 dbcon.connection.connect(function(err) {
     if (err) throw err;
     console.log("Connected!");
-    con.query("CREATE DATABASE mydb", function (err, result) {
+    dbcon.connection.query("CREATE DATABASE mydb", function (err, result) {
       if (err) throw err;
       console.log("Database created");
     });
